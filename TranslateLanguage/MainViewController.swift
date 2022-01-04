7ca39e82e2470009f9e0e0196d625425b5dae5e2
yongcheol Lee <yongcheol.dev@gmail.com>
2022-01-04 23:53:05 +0900

    Init project
    add package dependencies
    snapkit
    alamofire

--- TranslateLanguage/MainViewController.swift
+++ TranslateLanguage/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  TranslateLanguage
 //
 //  Created by yongcheol Lee on 2022/01/04.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
